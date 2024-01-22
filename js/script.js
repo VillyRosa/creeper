e19464aebbabe5b04d576040c51a077521cbef76
villy-r <villy.rosa@sigmasoftware.com.br>
2024-01-22 18:02:00 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,39 +1,35 @@
-var soundCreeper = new Audio('sounds/creeper-sound.wav');
-var creeper = window.document.querySelector('main');
-var body = window.document.querySelector('body');
-var egg;
+const explodeSound = new Audio('./sound/creeper-sound.wav');
+const creeper = document.querySelector('#creeper');
+const egg = document.querySelector('#egg');
 
-function explode() {
-    creeper.style.animation = `explode 5s`;
-    soundCreeper.play();
-    creeper.style.cursor = `default`;
+let eggClicks = 0;
+
+creeper.addEventListener('click', () => {
+    toggleClass(creeper, 'explode');
+    explodeSound.play();
     setTimeout(() => {
-        body.innerHTML = `<img class="egg" onclick="chocar()" src="images/egg.png">`;
-        egg = window.document.querySelector('img.egg');
-    }, 4500);
-}
+        toggleClass(creeper, 'hidden');
+        toggleClass(creeper, 'explode');
+        toggleClass(egg, 'hidden');
+    }, 4000);
+});
 
-var contador = 0;
-function chocar() {
-    if (contador == 0) {
-        egg.style.animation = "egg-1 2s";
-        contador++;
+egg.addEventListener('click', () => {
+    if (eggClicks === 0) {
+        toggleClass(egg, 'egg1Animate');
     } else {
-        egg.style.animation = "egg-2 2s";
-        contador = 0;
+        toggleClass(egg, 'egg2Animate');
         setTimeout(() => {
-            body.innerHTML = `<main onclick="explode()">
-                            <div class="eyes">
-                                <div class="eye" id="eye-left"></div>
-                                <div class="eye" id="eye-right"></div>
-                            </div>
-                            <div class="container-mouth">
-                                <div class="mouth" id="mouth-left"></div>
-                                <div class="mouth-center" id="mouth-center"></div>
-                                <div class="mouth" id="mouth-right"></div>
-                            </div>
-                        </main>`;
-            creeper = window.document.querySelector('main');
-        },2000)
+            toggleClass(egg, 'hidden');
+            toggleClass(creeper, 'hidden');
+            egg.classList.remove('egg1Animate');
+            egg.classList.remove('egg2Animate');
+        }, 2000)
     }
+    
+    eggClicks++;
+})
+
+function toggleClass(element, className) {
+    element.classList.toggle(className);
 }
